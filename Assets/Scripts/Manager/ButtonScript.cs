ce3af9093902c50a466c0d38f3e0fd43e09ae23b
rockatoo <youlia73@naver.com>
2019-03-26 19:15:48 +0900

    Added standard stage that show game functions
    Signed-off-by: rockatoo <youlia73@naver.com>

---
+++ Assets/Scripts/Manager/ButtonScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class ButtonScript : MonoBehaviour {
+
+	public void GoToStage(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+}
