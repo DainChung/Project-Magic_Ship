d630ae05ffb2b2fb6cb6eb9a377c51ce6cefbbf5
Dain Jeong <101_silence@naver.com>
2019-04-01 18:59:41 +0900

    EnemyIndicator Function Complete
    Signed-off-by: Dain Jeong <101_silence@naver.com>

--- Assets/Scripts/UI/EnemyIndicator.cs
+++ Assets/Scripts/UI/EnemyIndicator.cs
@@ -7,12 +7,16 @@ public class EnemyIndicator : MonoBehaviour {
     private Transform enemy;
     private Transform player;
 
+
     private EnemyUI enemyUI;
     private Transform mainCamera;
     private Transform mainCanvas;
 
+
     private Vector2 scrPos;
+
     private Vector3 destiVector3;
+    private float newDegree;
 
     private Vector3 FOriginalLocalScale;
 
@@ -36,19 +40,27 @@ public class EnemyIndicator : MonoBehaviour {
         {
             if(!(transform.GetComponent<Image>().enabled))    transform.GetComponent<Image>().enabled = true;
 
+            //일단 MainCamera를 바라보도록 한다.
             transform.rotation = mainCamera.rotation;
 
+            //enemyIndicator의 화면 상 위치에 대해 보정
             scrPos = Camera.main.WorldToScreenPoint(mainCanvas.position - Vector3.Normalize(mainCanvas.position - enemy.position) * 10f);
 
-            if (scrPos.x <= indicatorDist) { scrPos.x = indicatorDist; }
-            else if(scrPos.x >= Screen.width - indicatorDist) { scrPos.x = Screen.width - indicatorDist; }
+            if (scrPos.x <= indicatorDist + 5f) { scrPos.x = indicatorDist + 5f; }
+            else if(scrPos.x >= Screen.width - indicatorDist - 5f) { scrPos.x = Screen.width - indicatorDist - 5f; }
             if (scrPos.y <= 20f) { scrPos.y = indicatorDist; }
             else if(scrPos.y >= Screen.height - indicatorDist) { scrPos.y = Screen.height - indicatorDist; }
 
-            destiVector3.Set(scrPos.x, scrPos.y, 15f);
+            //enemyIndicator 위치 마지막 보정
+            destiVector3.Set(scrPos.x, scrPos.y, 10f);
 
-            transform.localScale = (3f * FOriginalLocalScale) / (Vector3.Distance(player.position, enemy.position) * 0.1f);
+            //enemyIndicator가 Enemy 방향을 가리키기 위한 보정
+            newDegree = (Quaternion.LookRotation(enemy.position - player.position, Vector3.up)).eulerAngles.y * (-1) + 45f;
+            //MainCamera를 바라보면서 Enemy 방향을 가리키기 위해 localRotation을 사용한다.
+            transform.localRotation = Quaternion.Euler(0,0, newDegree);
+            transform.localScale = (0.3f * FOriginalLocalScale) / (Vector3.Distance(player.position, enemy.position) * 0.02f);
 
+            //보정된 위치 값 적용
             transform.position = Camera.main.ScreenToWorldPoint(destiVector3);
         }
         else
